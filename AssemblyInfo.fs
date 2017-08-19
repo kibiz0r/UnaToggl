b0ab3b87bf24d93b37f98fd909774d0fcdde0167
Michael Harrington <kibiz0r@gmail.com>
2017-08-19 16:40:47 -0400

    First commit

---
+++ AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace UnaToggl
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("UnaToggl")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("${AuthorCopyright}")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
